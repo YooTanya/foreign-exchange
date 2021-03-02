113fda4be99f9a6c4a5bd18fa26eed486d287143
YooTanya <yootanya@gmail.com>
2021-03-02 20:08:38 +0700

    initiate data in table

--- src/components/pages/CounterStaff/RequestListTable.js
+++ src/components/pages/CounterStaff/RequestListTable.js
@@ -30,21 +30,11 @@ function createData(transactionID, customer, owner, status) {
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('UCL-2702213458', 'Rung Ngansanga', 'Tanya Rimphanitchayakit', 'PENDING'),
+  createData('UCL-2702213458', 'Namthip Pratanakrit', 'Tanya Rimphanitchayakit', 'APPROVED'),
+  createData('UCL-2702213458', 'Pongsak Kriangkrai', 'Worawut Songkhoal', 'REJECTED'),
+  createData('UCL-2702213458', 'Khanit Nuchnoi', 'Srichan Kuthivimol', 'PENDING'),
+  createData('UCL-2702213458', 'Suvimon Kunijvanit', 'Tanya Rimphanitchayakit', 'RESUBMITTED'),
 ];
 
 const useStyles = makeStyles({
