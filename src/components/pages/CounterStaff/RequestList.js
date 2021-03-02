2eca2408d4e3ed51e1c7257783582a3db364a7a8
YooTanya <yootanya@gmail.com>
2021-03-02 21:05:08 +0700

    displays data in table when clicked on refresh button

--- src/components/pages/CounterStaff/RequestList.js
+++ src/components/pages/CounterStaff/RequestList.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import RequestListHeader from './RequestListHeader';
 import RequestListEmpty from './RequestListEmpty';
 import RequestListTable from './RequestListTable';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+		paddingTop: theme.spacing(5)
+	},
+	header: {
+		marginBottom: theme.spacing(3)
+	},
+	body: {}
+}));
+
+
 const RequestList = () => {
-	const [requestList, setRequestList] = useState([]);
+  const classes = useStyles();
+	const [requestList, setRequestList] = useState(false);
+
+	const handleOnClickRefresh = () => {
+		setRequestList(true)
+	}
 
   return (
-		<>
-			<RequestListHeader />
-			{ requestList.lenght > 0 ? 
+		<div className={classes.root}>
+			<div className={classes.header}>
+				<RequestListHeader handleOnClickRefresh={handleOnClickRefresh}/>
+			</div>
+			<div className={classes.body}>
+				{ requestList ? 
 				<RequestListTable /> : <RequestListEmpty /> }
-		</>
+			</div>
+		
+		</div>
   );
 };
 
--- src/components/pages/CounterStaff/RequestListHeader.js
+++ src/components/pages/CounterStaff/RequestListHeader.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
-const RequestListHeader = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  buttons: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+  },
+}));
+
+const RequestListHeader = ({handleOnClickRefresh}) => {
+  const classes = useStyles();
+
   return (
 		<>
 			<Grid container justify="space-between">
-				<Grid class="item" xs={6}>
+				<Grid item xs={6}>
 					<Typography variant="h4">Request List</Typography>
 				</Grid>
-				<Grid class="item" xs={6} >
-					<Button variant="contained" color="primary">
-						New Request
-					</Button>
-          <Button variant="contained">
-						Refresh
-					</Button>
+				<Grid item xs={6}>
+					<Box className={classes.buttons}>
+						<Button variant="contained" color="primary">
+							New Request
+						</Button>
+						<Button variant="contained" onClick={handleOnClickRefresh}>
+							Refresh
+						</Button>
+					</Box>
 				</Grid>
 			</Grid>
 		</>
